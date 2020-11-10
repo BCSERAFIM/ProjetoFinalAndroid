dcd6c7edd6b71166fcc497cf2ef0f2e9c437b9ab
Bruno Cesar Serafim <bcserafim@gmail.com>
2020-11-10 11:57:11 -0300

    Inclusão das entidades

--- app/src/main/java/com/bcserafim/projetoandroid/activity/MainActivity.java
+++ app/src/main/java/com/bcserafim/projetoandroid/activity/MainActivity.java
@@ -1,7 +1,8 @@
-package com.bcserafim.projetoandroid;
+package com.bcserafim.projetoandroid.activity;
 
 import android.os.Bundle;
 
+import com.bcserafim.projetoandroid.R;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
--- app/src/main/java/com/bcserafim/projetoandroid/activity/SecondFragment.java
+++ app/src/main/java/com/bcserafim/projetoandroid/activity/SecondFragment.java
@@ -1,4 +1,4 @@
-package com.bcserafim.projetoandroid;
+package com.bcserafim.projetoandroid.activity;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -8,6 +8,8 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
+
+import com.bcserafim.projetoandroid.R;
 
 public class SecondFragment extends Fragment {
 
