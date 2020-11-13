a24108a533de44735a7485adc7e58e63e34c8df7
Carlos Eduardo <carloseduardobsi@gmail.com>
2020-11-12 22:06:21 -0300

    Salvando usuário

--- app/src/main/java/com/bcserafim/projetoandroid/service/ProdutoService.java
+++ app/src/main/java/com/bcserafim/projetoandroid/service/ProdutoService.java
@@ -3,12 +3,15 @@ package com.bcserafim.projetoandroid.service;
 import android.content.Context;
 
 import com.bcserafim.projetoandroid.entity.Produto;
+import com.bcserafim.projetoandroid.entity.Usuario;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 
 public interface ProdutoService {
 
@@ -16,5 +19,7 @@ public interface ProdutoService {
 
     @GET("produto")
     Call<List<Produto>> carregarProdutos();
+    @POST("usuario")
+    Call<Usuario> cadastrarUsuario(@Body Usuario usuario);
 
 }
